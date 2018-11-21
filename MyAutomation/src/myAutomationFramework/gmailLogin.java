853d6a5e15215d22a7225811a64c122045558cb6
RAIS <RAIS@RAIS-PC>
2018-11-22 01:15:58 +0530

    First Commit

---
+++ MyAutomation/src/myAutomationFramework/gmailLogin.java
@@ -0,0 +1,16 @@
+package myAutomationFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class gmailLogin {
+
+	public static void main(String[] args) {
+		
+		 String exePath = "D:\\Tech\\MyWorkspace\\chromedriver.exe";
+		 System.setProperty("webdriver.chrome.driver", exePath);
+		 WebDriver driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.get("http://toolsqa.wpengine.com/automation-practice-form/");
+		 }	
+}
