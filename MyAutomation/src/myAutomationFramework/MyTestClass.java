1e2fbddddbca9f2e2ccdbd8dcf824b6154263c93
RAIS <RAIS@RAIS-PC>
2018-11-22 21:44:54 +0530

    ok

--- MyAutomation/src/myAutomationFramework/MyTestClass.java
+++ MyAutomation/src/myAutomationFramework/MyTestClass.java
@@ -1,30 +1,38 @@
 package myAutomationFramework;
-
+ 
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+ 
 public class MyTestClass {
-	
-public WebDriver driver;
-	 
-@Test
-public void openMyBlog() {
-	driver.get("https://www.softwaretestingmaterial.com/");
-}
-	  
-@BeforeClass
-public void beforeClass() {
-	String exePath = "D:\\Tech\\MyWorkspace\\chromedriver.exe";
-	System.setProperty("webdriver.chrome.driver", exePath);
-	driver = new ChromeDriver();
-}
-
-@AfterClass
-public void afterClass() {
-	driver.quit();
-}
-	 
+ 
+ public WebDriver driver;
+ 
+  @Test
+ 
+  public void main() {
+      driver.findElement(By.id("account")).click();
+      driver.findElement(By.id("log")).sendKeys("testuser_1");
+      driver.findElement(By.id("pwd")).sendKeys("Test@123");
+      driver.findElement(By.id("login")).click();
+      System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
+      driver.findElement(By.id("account_logout"));
+  }
+ 
+  @BeforeMethod
+  public void beforeMethod() {
+      driver = new ChromeDriver();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      driver.get("http://www.onlinestore.toolsqa.wpengine.com");
+  }
+ 
+  @AfterMethod
+  public void afterMethod() {
+      driver.quit();
+  }
 }
